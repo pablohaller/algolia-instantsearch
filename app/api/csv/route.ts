890fe570b090b36c8df50f4318d6e2d6a31d6a02
pablo.haller.dev@gmail.com <pablo.haller.dev@gmail.com>
2024-03-19 22:48:00 -0300

    refactor: go back

--- app/api/csv/route.ts
+++ app/api/csv/route.ts
@@ -1,7 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { promises as fs } from "fs";
 
-export const runtime = "edge";
 export async function GET(req: NextRequest) {
   const authHeader = req.headers.get("authorization");
 
